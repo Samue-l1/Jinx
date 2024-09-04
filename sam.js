c6d47c52993a90c9b8ec4d4e4c9d77f220f4c29e
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-09-04 11:11:55 +0300

    Update sam.js

--- sam.js
+++ sam.js
@@ -1560,6 +1560,76 @@ case 'thunder':
 	sam.sendMessage(from, { image: { url: `https://api.lolhuman.xyz/api/textprome/${command}?apikey=cde5404984da80591a2692b6&text=${text}` } })
 	break; 
 //=================================================//
+case 'cry': case 'kill': case 'hug': case 'pat': case 'lick': 
+case 'kiss': case 'bite': case 'yeet': case 'bully': case 'bonk':
+case 'wink': case 'poke': case 'nom': case 'slap': case 'smile': 
+case 'wave': case 'awoo': case 'blush': case 'smug': case 'glomp': 
+case 'happy': case 'dance': case 'cringe': case 'cuddle': case 'highfive': 
+case 'shinobu': case 'handhold': {
+axios.get(`https://api.waifu.pics/sfw/${command}`)
+.then(({data}) => {
+sam.sendImageAsSticker(m.chat, data.url, m, { packname: packname, author: author })
+})
+}
+break;
+case 'wanumber': case 'nowa': case 'searchnumber':{
+                   if (!text) return reply(`📵🔢 Enter a number ending with 'x'\n\nExample: ${prefix + command} 254919933xx`)
+var inputnumber = text.split(" ")[0]
+
+        reply(`📱🔍 Exploring for WhatsApp accounts within the range...`)
+        function countInstances(string, word) {
+            return string.split(word).length - 1
+        }
+        var number0 = inputnumber.split('x')[0]
+        var number1 = inputnumber.split('x')[countInstances(inputnumber, 'x')] ? inputnumber.split('x')[countInstances(inputnumber, 'x')] : ''
+        var random_length = countInstances(inputnumber, 'x')
+        var randomxx
+        if (random_length == 1) {
+            randomxx = 10
+        } else if (random_length == 2) {
+            randomxx = 100
+        } else if (random_length == 3) {
+            randomxx = 1000
+        }
+        var text66 = `🔖 *WhatsApp Numbers Directory*\n\n`
+        var nobio = `\n*Bio:* || \nHey there! I am using WhatsApp.\n`
+        var nowhatsapp = `\n*Numbers with no WhatsApp account within provided range.*\n`
+        for (let i = 0; i < randomxx; i++) {
+            var nu = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+            var status1 = nu[Math.floor(Math.random() * nu.length)]
+            var status2 = nu[Math.floor(Math.random() * nu.length)]
+            var status3 = nu[Math.floor(Math.random() * nu.length)]
+            var dom4 = nu[Math.floor(Math.random() * nu.length)]
+            var random21
+            if (random_length == 1) {
+                random21 = `${status1}`
+            } else if (random_length == 2) {
+                random21 = `${status1}${status2}`
+            } else if (random_length == 3) {
+                random21 = `${status1}${status2}${status3}`
+            } else if (random_length == 4) {
+                random21 = `${status1}${status2}${status3}${dom4}`
+            }
+            var anu = await sam.onWhatsApp(`${number0}${i}${number1}@s.whatsapp.net`)
+            var anuu = anu.length !== 0 ? anu : false
+            try {
+                try {
+                    var anu1 = await sam.fetchStatus(anu[0].jid)
+                } catch {
+                    var anu1 = '401'
+                }
+                if (anu1 == '401' || anu1.status.length == 0) {
+                    nobio += `wa.me/${anu[0].jid.split("@")[0]}\n`
+                } else {
+                    text66 += `📞 *Number:* wa.me/${anu[0].jid.split("@")[0]}\n 🌐 ️*Bio :* ${anu1.status}\n⏳️*Last update :* ${moment(anu1.setAt).tz('Africa/Nairobi').format('HH:mm:ss DD/MM/YYYY')}\n\n`
+                }
+            } catch {
+                nowhatsapp += `${number0}${i}${number1}\n`
+            }
+        }
+        reply(`${text66}${nobio}${nowhatsapp}`)
+        }
+break;
  //===============================================//
 default:
 }
