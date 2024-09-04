2e511ba7ac7a5bc1fd7a08c2a83eb7243b9fb59b
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-09-04 13:54:39 +0300

    Update sam.js

--- sam.js
+++ sam.js
@@ -138,6 +138,7 @@ const { remini } = require("./lib/remini")
 const { toPTT, toAudio } = require("./lib/converter")
 const { UploadFileUgu, webp2mp4File, floNime, TelegraPh } = require("./lib/uploader")
 const { toTelegra } = require("./lib/upload")
+const photooxy = require('./lib/photooxy')
 //=================================================//
 const { groupMembers } = m
 var budy = (typeof m.text == "string" ? m.text : "")
@@ -1631,6 +1632,68 @@ var inputnumber = text.split(" ")[0]
         }
 break;
  //===============================================//
+case 'shadow': 
+case 'write': 
+case 'romantic': 
+case 'burnpaper':
+case 'smoke': 
+case 'narutobanner': 
+case 'love': 
+case 'undergrass':
+case 'doublelove': 
+case 'coffecup':
+case 'underwaterocean':
+case 'smokyneon':
+case 'starstext':
+case 'rainboweffect':
+case 'balloontext':
+case 'metalliceffect':
+case 'embroiderytext':
+case 'flamingtext':
+case 'stonetext':
+case 'writeart':
+case 'summertext':
+case 'wolfmetaltext':
+case 'nature3dtext':
+case 'rosestext':
+case 'naturetypography':
+case 'quotesunder':
+case 'shinetext':{
+
+if (!q) return reply(`🔮Example : ${prefix+command} King Sam`) 
+let link
+if (/stonetext/.test(command)) link = 'https://photooxy.com/online-3d-white-stone-text-effect-utility-411.html'
+if (/writeart/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/write-art-quote-on-wood-heart-370.html'
+if (/summertext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/3d-summer-text-effect-367.html'
+if (/wolfmetaltext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/create-a-wolf-metal-text-effect-365.html'
+if (/nature3dtext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/make-nature-3d-text-effects-364.html'
+if (/rosestext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/yellow-roses-text-360.html'
+if (/naturetypography/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/create-vector-nature-typography-355.html'
+if (/quotesunder/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/quotes-under-fall-leaves-347.html'
+if (/shinetext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/rainbow-shine-text-223.html'
+if (/shadow/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/shadow-text-effect-in-the-sky-394.html'
+if (/write/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/write-text-on-the-cup-392.html'
+if (/romantic/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/romantic-messages-for-your-loved-one-391.html'
+if (/burnpaper/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/write-text-on-burn-paper-388.html'
+if (/smoke/.test(command)) link = 'https://photooxy.com/other-design/create-an-easy-smoke-type-effect-390.html'
+if (/narutobanner/.test(command)) link = 'https://photooxy.com/manga-and-anime/make-naruto-banner-online-free-378.html'
+if (/love/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/create-a-picture-of-love-message-377.html'
+if (/undergrass/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/make-quotes-under-grass-376.html'
+if (/doublelove/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/love-text-effect-372.html'
+if (/coffecup/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/put-any-text-in-to-coffee-cup-371.html'
+if (/underwaterocean/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/creating-an-underwater-ocean-363.html'
+if (/smokyneon/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/make-smoky-neon-glow-effect-343.html'
+if (/starstext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/write-stars-text-on-the-night-sky-200.html'
+if (/rainboweffect/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/glow-rainbow-effect-generator-201.html'
+if (/balloontext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/royal-look-text-balloon-effect-173.html'
+if (/metalliceffect/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/illuminated-metallic-effect-177.html'
+if (/embroiderytext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/create-embroidery-text-online-191.html'
+if (/flamingtext/.test(command)) link = 'https://photooxy.com/logo-and-text-effects/realistic-flaming-text-effect-online-197.html'
+let dehe = await photooxy.photoOxy(link, q)
+sam.sendMessage(m.chat, { image: { url: dehe }, caption: `${isdone}` }, { quoted: m })
+}
+break;
+//=======================================================//
 default:
 }
 }
