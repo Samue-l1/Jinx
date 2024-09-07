75213db6bd7d964737d62e1d207f7e29051a32de
King Sam <141226873+Samue-l1@users.noreply.github.com>
2024-09-07 11:18:50 +0300

    Update config.js

--- derived/config.js
+++ derived/config.js
@@ -6,8 +6,8 @@
 */
 
 global.prefa = ['','!','.',',','🐤','🗿']
-global.owner = ['254742491666']
-global.ownMain = '254742491666'
+global.owner = ['27782182246']
+global.ownMain = '27782182246'
 global.NamaOwner = '𝕶𝖎𝖓𝖌 𝕾𝖆𝖒' //
 global.sessionName = 'session'
 global.connect = true // 
